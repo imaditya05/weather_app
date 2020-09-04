837d7326a211af23a64e42c587f55e0cf3f14857
imaditya05 <imaditya05@github.com>
2020-09-04 21:19:05 +0530

    Remove unncessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,6 @@
 
 
 
-console.log('Client side javascript fiel is loaded')
-
 fetch('http://puzzle.mead.io/puzzle').then((response) => {
     response.json().then((data)=>{
         console.log(data)
